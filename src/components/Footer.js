0615ba148af65653deb7ffc7baa565818d9eb3f5
Kenshin299 <kemylfernandez299@gmail.com>
2024-11-22 20:25:19 -0400

    Agregado footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <footer>
+            <div>
+                Muro Interactivo by Kemyl Fernandez
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
